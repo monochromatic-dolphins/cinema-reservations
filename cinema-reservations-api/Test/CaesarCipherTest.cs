519ec56b5d921811cef00c06bc73f4baa49087fc
Szymon Lipiec <lipieczsp6@gmail.com>
2021-11-02 18:54:59 +0100

    Add test class and test method

---
+++ cinema-reservations-api/Test/CaesarCipherTest.cs
@@ -0,0 +1,16 @@
+﻿using cinema_reservations_api.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cinema_reservations_api.Test {
+    
+    [TestClass]
+    public class CaesarCipherTest {
+        
+        [TestMethod]
+        public void TestEncode() {
+            var text = "admin";
+            var encodedText = "benjo";
+            Assert.AreEqual(CaesarCipher.Encode(text), encodedText);
+        }
+    }
+}
